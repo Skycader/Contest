b835f8ee87b04fb05d1c4cab6f6b7dcea91e9cf4
skycader <skycader@github.com>
2023-07-19 20:02:53 +0300

    changes

--- js-regexp/ex2.solution.js
+++ js-regexp/ex2.solution.js
@@ -3,7 +3,8 @@
  */
 
 const r =
-  /Mrs?./g; /*s? means that to find one s is optional*/
+  /Mrs\./g; /*s? means that to find one s is optional*/
+const r2 = /(Mr|Mrs)\./g;
 const f = (text) => {
   return text.replace(r, '');
 };
