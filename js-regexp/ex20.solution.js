d63a61f3fda5318b7b557ed004efb895424f8ec6
skycader <skycader@yandex.ru>
2024-05-31 00:03:01 +0300

    main: ex20 troubles"

--- js-regexp/ex20.solution.js
+++ js-regexp/ex20.solution.js
@@ -2,13 +2,22 @@
  * Make all texts within h1,2,3...6 tags CAPITAL
  */
 
+/** Solution 31.05.2024 00:02 */
+
+const f = (text) => {
+  const r = /<(h[1-6])>(.*?)<\/\1>/g;
+  return text.replace(r, (matrch, $1, $2) => {
+    return `<${$1}>${$2.toUpperCase()}</${$1}>`;
+  });
+};
+
 /** A better solution using back reference
  * 17 may 2024 12:38
  * */
-const f = (text) => {
+const f3 = (text) => {
   const r = /<h([1-6])>(.*?)<\/h\1>/g;
   return text.replace(r, (match, $1, $2) => {
-    return '<h' + $1 + '>' + $2.toUpperCase() + '</h' + $1 + '>';
+    return "<h" + $1 + ">" + $2.toUpperCase() + "</h" + $1 + ">";
   });
 };
 
