5efbeb8eb9f17c97452b3fed16d8a8c54cb290e2
skycader <skycader@yandex.ru>
2024-08-14 11:30:49 +0300

    main: ex19 refix

--- js-regexp/ex19.js
+++ js-regexp/ex19.js
@@ -1,6 +1,9 @@
 /**
  * Make sure the password consist of only ASCII symbols,
  * which is A-Z a-z 0-9 all special symbols including $
+ *
+ * Input: password only
+ * Output: boolean
  */
 
 module.exports = f;
