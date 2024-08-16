24f77f4d5c83626c177c8c94599af1bd8ce621c0
skycader <skycader@yandex.ru>
2024-08-16 23:38:11 +0300

    main: new

--- js-regexp/ex5.js
+++ js-regexp/ex5.js
@@ -2,8 +2,8 @@
  * Find people who own rat or cat and their name is just 4 letters length
  * -> Jane's cat and Mike's rat and Naomi's dog and Alex's cat and John's dog => Jane Mike Alex (3)
  */
-const r = //;
+const r = /((\w){4})'s\s[r|c]at/;
 const f = (text) => {
-  return;
+  return text.match(r)?.length || 0;
 };
 module.exports = f;
