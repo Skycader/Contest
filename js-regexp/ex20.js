e2b958d91d779eae89d9da8f04cecc2d7fa599e2
skycader <skycader@yandex.ru>
2024-08-19 11:32:25 +0300

    main: ex20 fix

--- js-regexp/ex20.js
+++ js-regexp/ex20.js
@@ -2,11 +2,6 @@
  * Make all texts within h1,2,3...6 tags CAPITAL
  */
 
-const f = (text) => {
-  const r = /<(h[1-6])>(.*?)<\/\1>/g;
-  return text.replace(r, (match, m1, m2) => {
-    return `<${m1}>${m2.toUpperCase()}</${m1}>`;
-  });
-};
+const f = (text) => {};
 
 module.exports = f;
