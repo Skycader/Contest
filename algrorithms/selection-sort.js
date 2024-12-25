89c09717056002550463dd9281996cf5ef807b81
Устименко Даниил <ustimeko_di@crc.ru>
2024-12-26 00:47:25 +0300

    main: ready

--- algrorithms/selection-sort.js
+++ algrorithms/selection-sort.js
@@ -7,18 +7,5 @@
  *
  */
 
-const f = (api) => {
-  let sorted_count = 0;
-  let min_index = 0;
-  const arrayLength = api.getLength();
-
-  for (let i = sorted_count; i < arrayLength; i++) {
-    for (let j = sorted_count; j < arrayLength; j++) {
-      if (api.get(min_index) > api.get(j)) min_index = j;
-    }
-    api.set(sorted_count, api.get(min_index));
-    sorted_count++;
-  }
-};
-
+const f = (api) => { };
 module.exports = f;
