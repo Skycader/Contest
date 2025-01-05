b32cdd9d5575787d2c7fa18daee201f35d0bf43f
Устименко Даниил <ustimeko_di@crc.ru>
2025-01-05 23:51:21 +0300

    main: ex24 welcome

---
+++ js-regexp/ex24.js
@@ -0,0 +1,9 @@
+/**
+ * Given any telephone number format,
+ * transform it into gigits only format.
+ * +7 987 231 45 23 => 79872314523
+ * +7 (987) 231 45-23 => 79872314523
+ * etc.
+ */
+
+module.exports = f;
