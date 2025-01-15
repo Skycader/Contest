6880cec5c28585cb4c95d75dd534fd9cf3b11423
Устименко Даниил <ustimeko_di@crc.ru>
2025-01-15 03:45:46 +0300

    main: add doc to sort

--- algrorithms/selection-sort.js
+++ algrorithms/selection-sort.js
@@ -5,7 +5,11 @@
  * const array = new Array()
  * f(array) => array is expected to be sorted
  *
+ * API methods:
+ * - get(index) => returns item by index
+ * - set(index,value) => set item by index
+ * - getLength() => returns array length
  */
 
-const f = (api) => { };
+const f = (api) => {};
 module.exports = f;
