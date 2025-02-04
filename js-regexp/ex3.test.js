557ac8c26a3223b8443acdab08cf8d14a51f024d
Устименко Даниил <ustimeko_di@crc.ru>
2025-02-04 17:38:29 +0300

    main: ex3

--- js-regexp/ex3.test.js
+++ js-regexp/ex3.test.js
@@ -3,7 +3,7 @@ f = require("./ex3.js");
 describe("Regexp", () => {
   it("Test case for ex3", () => {
     expect(f("Google Gooooooogle Gogle goooooooooooogle Yandex Googl")).toBe(
-      "Yandex Yandex Gogle goooooooooooogle Yandex Googl",
+      "Yandex Yandex Gogle Yandex Yandex Googl",
     );
   });
 });
