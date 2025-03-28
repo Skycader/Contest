44b700c18bd1f4b0126f15c6b443a2e41725a8c5
Устименко Даниил <ustimeko_di@crc.ru>
2025-03-28 12:55:22 +0300

    main: police better

--- algrorithms/police.test.js
+++ algrorithms/police.test.js
@@ -16,11 +16,27 @@ const testCase = {
   },
 };
 
-describe(":: Police Department Test", () => {
+const testCase2 = {
+  suspects: {
+    Алексей: ["Иван", "Наталья", "Сергей", "Игорь"],
+    Сергей: ["Иван", "Света"],
+    Наталья: ["Иван", "Света", "Сергей"],
+    Игорь: ["Иван", "Наталья"],
+  },
+  deadPeople: ["Иван", "Наталья"],
+};
+
+describe(":: Police Department Test Case #1", () => {
   it("should find the criminal", () => {
     expect(f(testCase.suspects, testCase.deadPeople)).toBe("Алексей");
   });
   it("should find the other criminal", () => {
     expect(f(testCase.suspects2, testCase.deadPeople)).toBe("Наталья");
+  });
+});
+
+describe(":: Police Department Test Case #2", () => {
+  it("should find the criminal", () => {
+    expect(f(testCase2.suspects, testCase.deadPeople)).toBe("Алексей");
   });
 });
