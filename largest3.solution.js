bd1e4d5328d08cb38c0c79b133f9a3de84b661f6
Устименко Даниил <ustimeko_di@crc.ru>
2025-06-19 22:16:02 +0300

    main: very declarative solution for largetst3.

--- largest3.solution.js
+++ largest3.solution.js
@@ -1,3 +1,11 @@
+/* 19 jul 2025 22:20 */
+
+const f = (str) =>
+  "9876543210"
+    .split("")
+    .map((i) => (str.includes(i.repeat(3)) ? i.repeat(3) : ""))
+    .filter(Boolean)[0] ?? "";
+
 /* 12 july 2023 22:27 GMT+3 */
 const f = (num) => {
   let str = String(num);
@@ -7,7 +15,7 @@ const f = (num) => {
       max = i;
     }
   }
-  if (max === -1) return '';
+  if (max === -1) return "";
   return max.toString().repeat(3);
 };
 
@@ -16,13 +24,12 @@ let f = (str) => {
   let max = -1;
   for (let i = 0; i < 10; i++) {
     if (str.indexOf(i.toString().repeat(3)) > -1) {
-      if (max < i.toString().repeat(3) * 1)
-        max = i.toString().repeat(3) * 1;
+      if (max < i.toString().repeat(3) * 1) max = i.toString().repeat(3) * 1;
     }
   }
-  if (max == 0) max = '000';
+  if (max == 0) max = "000";
   if (max != -1) return max.toString();
-  return '';
+  return "";
 };
 
 //05.december.2022
@@ -32,5 +39,5 @@ const f = (num) => {
       return i.toString().repeat(3);
     }
   }
-  return '';
+  return "";
 };
