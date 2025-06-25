d111841dad349ac39e8dbcb977cb3c6a4a306fb7
Устименко Даниил <ustimeko_di@crc.ru>
2025-06-25 18:30:13 +0300

    main: police

--- algrorithms/police.solution.js
+++ algrorithms/police.solution.js
@@ -1,3 +1,8 @@
+const f = (suspects, dead) =>
+  Object.entries(suspects).find(([suspect, seen]) =>
+    dead.every((dead) => seen.includes(dead)),
+  )[0];
+
 const f = (suspects, deadPeople) => {
   for (let [suspect, seenPeople] of Object.entries(suspects)) {
     if (deadPeople.every((deadMan) => seenPeople.includes(deadMan)))
