faf0da8c0babe070d55c36a0a3d1b30b88bb4050
Устименко Даниил <ustimeko_di@crc.ru>
2025-08-12 23:58:05 +0300

    main: water battles update

--- waterBottles.js
+++ waterBottles.js
@@ -1,9 +1,4 @@
-//given 9 water bottles, you can exchange 3 for a new full one. Count as many bottles you can drink.
-//f(9,3) => 13
-//f(15,4) => 19 (15,4,0,0) => (0,4,15,0) => (3,4,3,3) => (0,4,6,3) => (1,4,2,4) => (0,4,3,4)
+//given FULL water bottles, you can exchange PRICE for a new full one. Count as many bottles you can drink.
+//If given 15 full and price is 4: (15,4) => 19 (15,4,0,0) => (0,4,15,0) => (3,4,3,3) => (0,4,6,3) => (1,4,2,4) => (0,4,3,4)
 
-//const ff = decorator(f)
 module.exports = f;
-
-//console.log(f(9,3))
-//console.log(f(15,4))
