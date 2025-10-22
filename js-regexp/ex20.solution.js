7e4ad8fe26b897cfda0336f603d3ab3f1933a59a
Skycader <skycader@yandex.ru>
2025-10-22 18:00:33 +0300

    ok

--- js-regexp/ex20.solution.js
+++ js-regexp/ex20.solution.js
@@ -6,7 +6,7 @@ const f = (text) => {
   const r = /<(h[1-6])>(.*?)<\/\1>/g;
   return text.replace(
     r,
-    (match, m1, m2) => `<${m1}>${m2.toUpperCase()}</${m1}>`,
+    (match, m1, m2) => `<${m1}>${m2.toUpperCase()}</${m1}>`
   );
 };
 
@@ -31,9 +31,18 @@ const f3 = (text) => {
 
 const f2 = (text) => {
   const r = /(<h[1-6]>)(.*?)(<\/h[1-6]>)/gm;
-  return text.replace(r, function(match, $1, $2, $3) {
+  return text.replace(r, function (match, $1, $2, $3) {
     return $1 + $2.toUpperCase() + $3;
   });
+};
+
+//22.10.2025
+const f = (text) => {
+  const r = /<(h[1-6])>(.*?)<\/(\1)>/g;
+  return text.replace(
+    r,
+    (match, r1, r2, r3) => `<${r1}>${r2.toUpperCase()}</${r3}>`
+  );
 };
 
 module.exports = f;
