7095ce1333bf0fbdd8678ff8d191fd11a955bb73
Skycader <skycader@yandex.ru>
2025-11-05 16:42:19 +0300

    main: ex30 ok

--- js-regexp/ex30.js
+++ js-regexp/ex30.js
@@ -2,6 +2,8 @@
  * Among the given text,
  * find all the strings that could be good passwords,
  * that must have capital and lower case, one digit and one of !@#$%^&*
+ * Minimal length is 8 symbols
+ * Password must NOT have spaces
  * Find all of them
  */
 
