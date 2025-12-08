8024abe37210e0297598fd6c9461d42b908c96f9
Skycader <skycader@yandex.ru>
2025-12-09 00:44:03 +0300

    ok

--- js-regexp/ex30.solution.js
+++ js-regexp/ex30.solution.js
@@ -5,4 +5,10 @@ const f = (text) => {
   return text.match(r) || [];
 };
 
+const f = (text) => {
+  const r =
+    /(?=\S*[A-Z]\S*)(?=\S*[a-z]\S*)(?=\S*[0-9]\S*)(?=\S*[!@#$%^&*]\S*)[A-Za-z0-9!@#$%^&*]{8,}/g;
+  return text.match(r) || [];
+};
+
 module.exports = f;
