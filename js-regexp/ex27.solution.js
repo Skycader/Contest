27eb3a6e9ed1d8215ee410adc04c78b8b7d510ef
Skycader <skycader@yandex.ru>
2026-01-07 18:48:51 +0300

    main: update

--- js-regexp/ex27.solution.js
+++ js-regexp/ex27.solution.js
@@ -5,6 +5,11 @@
  * 3) 3 or more digits in a given string
  */
 
+//Shorter sol 07.01.2026
+const r1 = /^(\D*\d\D*){3}$/;
+const r2 = /^(\D*\d\D*){1,2}$/;
+const r3 = /^(\D*\d\D*){3,}$/;
+
 const r1 = /^(?=(?:\D*\d){3}\D*$)/; //<-- \D* - without these will work: exactly123, however, not this e123xactly
 const r2 = /^(?=(?:\D*\d){1,2}\D*$)/;
 const r3 = /^(?=(?:\D*\d){3,}\D*$)/;
